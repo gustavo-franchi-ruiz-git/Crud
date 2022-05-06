823ab29a7d3d7a13d7cdb2c0ede272d95ff488fe
Gustavo <gustavo.ruiz@sienacompany.com>
2022-05-06 08:08:09 -0300

    Adicionado validação do email na edição de campo.

--- js/editavel.js
+++ js/editavel.js
@@ -1,10 +1,10 @@
 var campoNome = $(".info-nome")
 var campoSobrenome = $(".info-sobrenome")
-var campoEmail = $(".info-email")
+var campoEmail =$(".info-email")
 
 campoNome.dblclick(editarTd)
 campoSobrenome.dblclick(editarTd)
-campoEmail.dblclick(editarTd)
+campoEmail.dblclick(editarTdEmail)
 
 
 function editarTd() {
@@ -14,15 +14,49 @@ function editarTd() {
     $(this).html("<input type='text' value='" + conteudoOriginal + "' />");
     $(this).children().first().focus();
 
+
     $(this).children().first().keypress(function (e) {
         if (e.which == 13) {
-            var novoConteudo = $(this).val();
-            $(this).parent().text(novoConteudo);
-            $(this).parent().removeClass("celulaEmEdicao");
-        }
-    });
+            if ($(this).val().length != 0) {
+                var novoConteudo = $(this).val();
+                $(this).parent().text(novoConteudo);
+                $(this).parent().removeClass("celulaEmEdicao");
 
-$(this).children().first().blur(function(){
-    $(this).parent().text(conteudoOriginal);
-    $(this).parent().removeClass("celulaEmEdicao");
-})}
+                $(this).children().first().blur(function () {
+                    $(this).parent().text(conteudoOriginal);
+                    $(this).parent().removeClass("celulaEmEdicao");
+                })
+            }
+            else {
+                alert("Campo não pode ser em branco!")
+            }
+        }
+    })
+}
+
+function editarTdEmail() {
+    var conteudoOriginal = $(this).text();
+
+    $(this).addClass("celulaEmEdicao");
+    $(this).html("<input type='email' value='" + conteudoOriginal + "' />");
+    $(this).children().first().focus();
+
+
+    $(this).children().first().keypress(function (e) {
+        if (e.which == 13) {
+            if (validateEmail($(this).val())) {
+                var novoConteudo = $(this).val();
+                $(this).parent().text(novoConteudo);
+                $(this).parent().removeClass("celulaEmEdicao");
+
+                $(this).children().first().blur(function () {
+                    $(this).parent().text(conteudoOriginal);
+                    $(this).parent().removeClass("celulaEmEdicao");
+                })
+            }
+            else {
+                alert("Campo email está incorreto!")
+            }
+        }
+    })
+}
