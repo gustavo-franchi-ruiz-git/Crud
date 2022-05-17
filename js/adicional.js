1c5cced1df27f050c328de26eb5b2469c44c1279
Gustavo <gustavo.ruiz@sienacompany.com>
2022-05-17 14:40:12 -0300

    a

--- js/adicional.js
+++ js/adicional.js
@@ -54,7 +54,6 @@ function novoDado(nome, sobrenome, email){
 }
 
 function removeLinha(){
-    event.preventDefault();
     var linha = $(this).parent().parent()
     linha.fadeOut(1000);
     setTimeout(function () {
